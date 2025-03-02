c6b79350004114653adff9c134f134781cd5b5e0
Josiah4191 <josiah4191@gmail.com>
2025-03-02 16:10:10 -0600

    Refactored: Separated the map of images to a new class 'UnoCardClassicImages.' Reduced the code in the getImage() method.

--- src/model/cards/uno/UnoCardImageManager.java
+++ src/model/cards/uno/UnoCardImageManager.java
@@ -1,12 +1,5 @@
 package model.cards.uno;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static java.util.Map.entry;
-import static model.cards.uno.UnoSuit.*;
-import static model.cards.uno.UnoValue.*;
-
 /*
     1. This class is responsible for managing the images for our cards.
     2. When we create an UnoCardImageManager object, we can pass an UnoCard object to the getImage() method, which
@@ -22,7 +15,7 @@ import static model.cards.uno.UnoValue.*;
         - Add the new theme to the switch statement in getImage() so it pulls image from the appropriate map
 
     Note:
-        Right now, getImage() is returning a String. That's because I don't have images yet. To actually implement the images, I
+        Right now, getImage() is returning a String for each file. That's because I don't have images yet. To actually implement the images, I
         can think of a couple options:
             - We can still return a String which will be a file path of the image.
             - We can return an Image object from the javaFX library.
@@ -37,15 +30,11 @@ public class UnoCardImageManager {
     // Create and initialize the theme to classic
     private UnoCardTheme theme = UnoCardTheme.CLASSIC;
 
-    public UnoCardImageManager() {
-    }
-
     // Returns the Image. Accepts a UnoCard card object for the parameter
     public String getImage(UnoCard card) {
         switch (theme) { // Switch on the current theme
             case UnoCardTheme.CLASSIC: // If it's classic theme
-                var images = classicImages.get(card.getSuit()); // Get all the map entries from the cards suit
-                return images.get(card.getValue()); // Return the image that corresponds to the value of the card with that suit
+                return UnoCardClassicImages.IMAGES.get(card.getSuit()).get(card.getValue()); // Get the card suit key from the map of images, and then get the card value key from the inner map
             default:
                 return "";
         }
@@ -53,79 +42,7 @@ public class UnoCardImageManager {
 
     // This method sets the theme of the Images to be returned.
     public void setTheme(UnoCardTheme theme) {
-        this.theme = theme;
+        theme = theme;
     }
 
-    /* This is a Map that contains all the Images for each Uno card
-
-       The outer map keys are RED, GREEN, BLUE, YELLOW, WILD.
-       The outer map values are maps.
-
-       The inner map keys correspond to the card values in their suit.
-       The inner map values are the images for that value.
-     */
-
-    // @formatter:off
-    private static Map<UnoSuit, Map<UnoValue, String>> classicImages = new HashMap<>(Map.of(
-            RED, new HashMap<>(Map.ofEntries(
-                    entry(ZERO, "RED ZERO IMAGE"),
-                    entry(ONE, "RED ONE IMAGE"),
-                    entry(TWO, "RED TWO IMAGE"),
-                    entry(THREE, "RED THREE IMAGE"),
-                    entry(FOUR, "RED FOUR IMAGE"),
-                    entry(FIVE, "RED FIVE IMAGE"),
-                    entry(SIX, "RED SIX IMAGE"),
-                    entry(SEVEN, "RED SEVEN IMAGE"),
-                    entry(EIGHT, "RED EIGHT IMAGE"),
-                    entry(NINE, "RED NINE IMAGE"),
-                    entry(SKIP, "RED SKIP IMAGE"),
-                    entry(REVERSE, "RED REVERSE IMAGE"),
-                    entry(DRAW_TWO, "RED DRAW TWO IMAGE"))),
-            GREEN, new HashMap<>(Map.ofEntries(
-                    entry(ZERO, "GREEN ZERO IMAGE"),
-                    entry(ONE, "GREEN ONE IMAGE"),
-                    entry(TWO, "GREEN TWO IMAGE"),
-                    entry(THREE, "GREEN THREE IMAGE"),
-                    entry(FOUR, "GREEN FOUR IMAGE"),
-                    entry(FIVE, "GREEN FIVE IMAGE"),
-                    entry(SIX, "GREEN SIX IMAGE"),
-                    entry(SEVEN, "GREEN SEVEN IMAGE"),
-                    entry(EIGHT, "GREEN EIGHT IMAGE"),
-                    entry(NINE, "GREEN NINE IMAGE"),
-                    entry(SKIP, "GREEN SKIP IMAGE"),
-                    entry(REVERSE, "GREEN REVERSE IMAGE"),
-                    entry(DRAW_TWO, "GREEN DRAW TWO IMAGE"))),
-            BLUE, new HashMap<>(Map.ofEntries(
-                    entry(ZERO, "BLUE ZERO IMAGE"),
-                    entry(ONE, "BLUE ONE IMAGE"),
-                    entry(TWO, "BLUE TWO IMAGE"),
-                    entry(THREE, "BLUE THREE IMAGE"),
-                    entry(FOUR, "BLUE FOUR IMAGE"),
-                    entry(FIVE, "BLUE FIVE IMAGE"),
-                    entry(SIX, "BLUE SIX IMAGE"),
-                    entry(SEVEN, "BLUE SEVEN IMAGE"),
-                    entry(EIGHT, "BLUE EIGHT IMAGE"),
-                    entry(NINE, "BLUE NINE IMAGE"),
-                    entry(SKIP, "BLUE SKIP IMAGE"),
-                    entry(REVERSE, "BLUE REVERSE IMAGE"),
-                    entry(DRAW_TWO, "BLUE DRAW TWO IMAGE"))),
-            YELLOW, new HashMap<>(Map.ofEntries(
-                    entry(ZERO, "YELLOW ZERO IMAGE"),
-                    entry(ONE, "YELLOW ONE IMAGE"),
-                    entry(TWO, "YELLOW TWO IMAGE"),
-                    entry(THREE, "YELLOW THREE IMAGE"),
-                    entry(FOUR, "YELLOW FOUR IMAGE"),
-                    entry(FIVE, "YELLOW FIVE IMAGE"),
-                    entry(SIX, "YELLOW SIX IMAGE"),
-                    entry(SEVEN, "YELLOW SEVEN IMAGE"),
-                    entry(EIGHT, "YELLOW EIGHT IMAGE"),
-                    entry(NINE, "YELLOW NINE IMAGE"),
-                    entry(SKIP, "YELLOW SKIP IMAGE"),
-                    entry(REVERSE, "YELLOW REVERSE IMAGE"),
-                    entry(DRAW_TWO, "YELLOW DRAW TWO IMAGE"))),
-            UnoSuit.WILD, new HashMap<>(Map.ofEntries(
-                    entry(UnoValue.WILD, "WILD IMAGE"),
-                    entry(UnoValue.WILD_DRAW_FOUR, "WILD DRAW FOUR IMAGE")))
-    ));
-    // @formatter:on
 }
